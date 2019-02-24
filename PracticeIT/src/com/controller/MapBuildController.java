12540f72408b4bb9db039180335c2ddecb1b0139
si_sharm <si_sharm@COPPOLA.ENCS.concordia.ca>
2019-02-24 15:32:59 -0500

    Reading and displaying Map with modifications.

--- PracticeIT/src/com/controller/MapBuildController.java
+++ PracticeIT/src/com/controller/MapBuildController.java
@@ -1,36 +1,43 @@
 package com.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import com.main.MapSStep;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+//import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
+//import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MapBuildController implements EventHandler<ActionEvent>{
 	
-	 Parent root ;
+	
 	 Stage stage = new Stage();
 	public Button button;
 	@Override
 	public void handle(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		//Parent root ;
-		try {
-			   root = FXMLLoader.load(getClass().getResource("MapEditorNew.fxml"));
+		
+		File mapReturnedFile = MapSStep.mapFileValidator();
+		 MapSStep mapsstep=new MapSStep();
+		 try {
+			mapsstep.readingMapFile(mapReturnedFile);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        stage.setTitle("FXML Welcome");
+        try {
+			stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MapEditorNew.fxml"))));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-        Scene scene = new Scene(root);
-    
-        stage.setTitle("FXML Welcome");
-        stage.setScene(scene);
         stage.setResizable(true);
         stage.show(); 
 	
