8bd9b00bc7d82004bf6306ff879b48850d37f7ca
si_sharm <si_sharm@COLOMBIA.ENCS.concordia.ca>
2019-02-25 21:17:02 -0500

    handling exceptions.

---
+++ PracticeIT/src/com/main/newMapCreation.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class newMapCreation implements EventHandler<ActionEvent> {
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
