7f6277b07bde5b9bfbe50a34ba1e197fc7932457
s_gant <s_gant@MACEDONIA.ENCS.concordia.ca>
2019-03-20 18:26:55 -0400

    Fortification- Not completed

--- PracticeIT/src/com/model/GamePlan.java
+++ PracticeIT/src/com/model/GamePlan.java
@@ -1,12 +1,16 @@
 package com.model;
 
 import com.units.Territories;
+import com.units.Contestant;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 /**
  * The Class GamePlan is used to build strategy of attacking.
+ * @author sathwik
  */
-public abstract class GamePlan {
+public interface GamePlan {
 
 	/**
 	 * Attack phase.
@@ -17,6 +21,10 @@ public abstract class GamePlan {
 	public abstract void attackPhase(ListView<Territories> attackingTerritoryList, ListView<Territories> defendingTerritoryList,
 			PlayersAssignment gamePhase) ;
 
+	
+	public abstract void reinforcementPhase(ListView<Territories> territoryList, Territories territory,	Contestant pPlay);
+	
+	boolean fortificationPhase(ListView<Territories> selectedTerritory, ListView<Territories> adjTerritory, Contestant pPlay);
 	
 
 	
