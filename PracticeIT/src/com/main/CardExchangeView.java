52abe377a37ee436ed0904514c4bd16412eff1fd
Pritpal Kaur <pritkaur97@gmail.com>
2019-04-09 16:09:46 -0400

    java documentation

--- PracticeIT/src/com/main/CardExchangeView.java
+++ PracticeIT/src/com/main/CardExchangeView.java
@@ -6,19 +6,35 @@ import Patterns.Observable;
 import Patterns.Observer;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CardExchangeView.
+ */
 public class CardExchangeView implements Observer {
 
+	/** The obs. */
 	public Observable obs;
 	
-	 public CardExchangeView(Observable obs){
+	 /**
+ 	 * Instantiates a new card exchange view.
+ 	 *
+ 	 * @param obs the obs
+ 	 */
+ 	public CardExchangeView(Observable obs){
 		 Observer obs1=new CardExchangeView();
 		 System.out.println(obs1);
 		 this.obs=obs;
 		 obs.registerObserver(obs1);
 	 }
 	 
-	 public CardExchangeView() {}
+	 /**
+ 	 * Instantiates a new card exchange view.
+ 	 */
+ 	public CardExchangeView() {}
 	 
+	/* (non-Javadoc)
+	 * @see Patterns.Observer#update(java.lang.Object)
+	 */
 	@Override
 	public void update(Object arg1) {
 		 System.out.println("The player has been assigned"+PlayersAssignment.CardCount+"of type" +PlayersAssignment.cardtype);
